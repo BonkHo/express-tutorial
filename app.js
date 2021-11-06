d8226e8356a1f5d3bafc2111318cae331d9eea13
John Bae <john21225@gmail.com>
2021-11-05 19:47:53 -0700

    Simple server created

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Express Tutorial')
+const http = require("http");
+
+const server = http.createServer(function (req, res) {
+	res.write("Hello World");
+	console.log("User hit server.");
+	res.end();
+});
+
+server.listen(5000);
+console.log("Server is running on: http://localhost:5000");
