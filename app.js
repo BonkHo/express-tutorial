0d0bd4935cb6cc1077da85fe787deccb1a6487b3
John Bae <john21225@gmail.com>
2021-11-06 11:09:55 -0700

    Server urls setup using req object

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const http = require("http");
 
 const server = http.createServer(function (req, res) {
-	res.write("Hello World");
-	console.log("User hit server.");
-	res.end();
+	const url = req.url;
+	if (url === "/") {
+		res.writeHead(200, { "Content-Type": "text/html" });
+		res.write("<h1>Home Page</h1> <p>Welcome to the site.</p>");
+		res.end();
+	} else if (url === "/about") {
+		res.writeHead(200, { "Content-Type": "text/html" });
+		res.write("<h1>About Page</h1> <p>Welcome to the about page.</p>");
+		res.end();
+	} else {
+		res.writeHead(404, { "Content-Type": "text/html" });
+		res.write(
+			"<h1>404 Page Not Found</h1> <p>The page you are looking for does not exist.</p>"
+		);
+		res.end();
+	}
 });
 
 server.listen(5000);
