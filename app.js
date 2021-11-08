16a6868880e6faac8d040cc9a90f82df4f16e851
John Bae <john21225@gmail.com>
2021-11-07 16:27:46 -0800

    HTML linked to server

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+// Get all files
+const homePage = readFileSync("./index.html");
 
 const server = http.createServer(function (req, res) {
 	const url = req.url;
+	// Home Page
 	if (url === "/") {
 		res.writeHead(200, { "Content-Type": "text/html" });
-		res.write("<h1>Home Page</h1> <p>Welcome to the site.</p>");
+		res.write(homePage);
 		res.end();
+		// About Page
 	} else if (url === "/about") {
 		res.writeHead(200, { "Content-Type": "text/html" });
 		res.write("<h1>About Page</h1> <p>Welcome to the about page.</p>");
 		res.end();
+		// 404 Page
 	} else {
 		res.writeHead(404, { "Content-Type": "text/html" });
 		res.write(
