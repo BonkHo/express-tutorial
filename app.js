29b0f5508857ed0f7b0f32f265d5973370704506
John Bae <john21225@gmail.com>
2021-11-10 16:20:57 -0800

    Added status codes

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express();
 
 // app.get
 app.get("/", (req, res) => {
-	res.send("Home Page");
+	res.status(200).send("Home Page");
 	res.end();
 });
 
 app.get("/about", (req, res) => {
-	res.send("About Page");
+	res.status(200).send("About Page");
 	res.end();
 });
 
@@ -17,7 +17,7 @@ app.get("/about", (req, res) => {
 // app.delete
 // app.all
 app.all("*", (req, res) => {
-	res.send("404 Not Found");
+	res.status(404).send("404 Not Found");
 	res.end();
 });
 // app.use
