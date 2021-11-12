6a90f16560b49ef87f92134003c59d9b2efcc67c
John Bae <john21225@gmail.com>
2021-11-11 18:22:41 -0800

    Basic structuring of json files

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
 
+const { products } = require("./data");
+
 app.get("/", (req, res) => {
-	res.status(500).json([{ name: "john" }, { name: "susan" }]);
+	res.send("<h1> Home Page </h1><a href='./api/products'> Products </a>");
+});
+
+app.get("/api/products", (req, res) => {
+	const newProducts = products.map((product) => {
+		const { id, name, image } = product;
+		return { id, name, image };
+	});
+	res.json(newProducts);
+});
+
+app.all("*", (req, res) => {
+	res.send("<h1> Page Not Found </h1>");
 });
 
 app.listen(5000, () => {
